6bc2af59d90be9c28e57049ca1ae82225cf3c61f
alaa-am1305148 <alaamousa123456@gmail.com>
2019-04-08 12:14:17 +0300

    v-3

---
+++ mobile-app-code/v-3/materiallogindemo-master/app/src/main/java/com/sourcey/materiallogindemo/Reservation.java
@@ -0,0 +1,80 @@
+package com.sourcey.materiallogindemo;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * Created by Alaa on 10/25/2018.
+ */
+
+public class Reservation implements Serializable {
+
+    int resNo ;
+    String zoneName, date ,carPlateNo , status, uid ;
+    double price;
+    List<Integer> time;
+    int extendedHours, cancelledHours;
+    public Reservation() {
+    }
+
+    public Reservation(int resNo, String carPlateNo, String zoneName, String data, List<Integer> time, String status, double price,  int extendedHours,int cancelledHours, String uid) {
+        this.resNo = resNo;
+        this.carPlateNo = carPlateNo;
+        this.zoneName = zoneName;
+        this.date = data;
+        this.time = time;
+        this.status = status;
+        this.price = price;
+        this.extendedHours = extendedHours;
+        this.cancelledHours = cancelledHours;
+        this.uid = uid ;
+    }
+
+    public int getResNo() {
+        return resNo;
+    }
+
+    public String getCarPlateNo() {
+        return carPlateNo;
+    }
+
+    public String getZoneName() {
+        return zoneName;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public List<Integer> getTime() {
+        return time;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getExtendedHours() {
+        return extendedHours;
+    }
+
+    public void setExtendedHours(int extendedHours) {
+        this.extendedHours = extendedHours;
+    }
+
+    public int getCancelledHours() {
+        return cancelledHours;
+    }
+
+    public void setCancelledHours(int cancelledHours) {
+        this.cancelledHours = cancelledHours;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+}
